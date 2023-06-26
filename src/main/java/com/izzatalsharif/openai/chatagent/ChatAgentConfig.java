baa25d325793da10c89dc23d0455cb4e8efb8963
izzat5233 <izzat5233@gmail.com>
2023-06-26 23:05:46 +0300

    Spring configuration fix

--- src/main/java/com/izzatalsharif/openai/chatagent/ChatAgentConfig.java
+++ src/main/java/com/izzatalsharif/openai/chatagent/ChatAgentConfig.java
@@ -1,9 +1,11 @@
 package com.izzatalsharif.openai.chatagent;
 
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootConfiguration
+@EnableAutoConfiguration
 @ComponentScan
 public class ChatAgentConfig {
 }
